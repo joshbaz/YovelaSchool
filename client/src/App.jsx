d74479ba24d98b57c6558a9af8246b9f840d307d
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2023-05-02 22:01:16 +0300

    route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,20 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   const theme = useMemo(() => createTheme(themeSettings()), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+
+          <Routes>
+            <Route path="/" element={<div>home</div>} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
