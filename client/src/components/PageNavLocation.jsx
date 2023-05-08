383ffa50b4f6f3d2c3f9935d9b3fe1c5e13d1ade
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2023-05-08 20:13:26 +0300

    About Ace update

--- client/src/components/PageNavLocation.jsx
+++ client/src/components/PageNavLocation.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import CustomStack from "./CustomStack";
 import { useTheme, Box, Typography } from "@mui/material";
-const PageNavLocation = () => {
-    const { palette } = useTheme();
+import { Icon } from "@iconify/react";
+const PageNavLocation = ({ currlocation, prevlocation }) => {
+  const { palette } = useTheme();
   return (
-    <div>PageNavLocation</div>
-  )
-}
+    <CustomStack
+      padding={"7px 77px"}
+      flexDirection="row"
+      gap="10px"
+      bgcolor={palette.whites[100]}
+      alignItems={"center"}
+    >
+      <Typography
+        variant="body1"
+        color={palette.secondary[500]}
+        sx={{
+          textTransform: "uppercase",
+          textAlign: "center",
+          lineHeight: "26px",
+          padding: "",
+        }}
+      >
+        {prevlocation}
+      </Typography>
+      <Box width="30px" height="20px">
+        <Icon icon="bi:arrow-right-short" />
+      </Box>
+
+      <Typography
+        variant="body1"
+        color={palette.tertiary[200]}
+        sx={{
+          textTransform: "uppercase",
+          fontWeight: "600",
+          fontSize: "13px",
+          textAlign: "center",
+          lineHeight: "26px",
+        }}
+      >
+        {currlocation}
+      </Typography>
+    </CustomStack>
+  );
+};
 
 export default PageNavLocation
--- client/src/screens/aboutace/index.jsx
+++ client/src/screens/aboutace/index.jsx
@@ -5,6 +5,8 @@ import Navbar from "../../components/Navbar";
 import PageHeader from "../../components/PageHeader";
 
 import aboutaceimg from "../../assets/AboutAce.png";
+import PageNavLocation from "../../components/PageNavLocation";
+import Footer from "../../components/Footer";
 const Container = styled(Stack)(({ theme }) => ({
   textDecoration: "none",
   listStyle: "none",
@@ -19,11 +21,81 @@ const Container = styled(Stack)(({ theme }) => ({
 const AboutAce = () => {
   const { palette } = useTheme();
   return (
-    <Container spacing={0}>
-      <CustomStack flexDirection={"column"} height="100% ">
+    <Container spacing={0} gap="0px">
+      <CustomStack gap="0px" flexDirection={"column"}>
         <Navbar />
         <PageHeader title={"About ACE"} headerimage={aboutaceimg} />
       </CustomStack>
+
+      {/** nav pager */}
+      <PageNavLocation
+        currlocation={"Academics : Ace Curriculum"}
+        prevlocation={"Home"}
+      />
+
+      {/** ace video */}
+
+      {/** what ace is */}
+      <CustomStack
+        flexDirection={"column"}
+        width="100%"
+        alignItems="center"
+        padding="43px 0"
+        bgcolor={palette.tertiary[600]}
+        gap="25px"
+      >
+        <Typography
+          variant="body1"
+          color={palette.primary[50]}
+          sx={{
+            fontSize: "50px",
+            fontWeight: "200",
+            textTransform: "capitalize",
+          }}
+        >
+          What Is A.C.E?
+        </Typography>
+
+        <Typography
+          variant="body1"
+          color={palette.whites[500]}
+          sx={{
+            maxWidth: "811px",
+            textAlign: "center",
+            fontSize: "20px",
+            fontWeight: "200",
+          }}
+        >
+          The Accelerated Christian Education Curriculum Is A Unique Program
+          That Emphasizes Biblical Principle Along With Strong Academic Skills.
+          ACE Recognizes That Each Child Has A Unique Learning Style With
+          Individual Abilities and Talents.ACE is evaluated by the International
+          Certificate of Christian Education (ICCE) which awards certificates to
+          the learners
+        </Typography>
+      </CustomStack>
+
+      {/** levels of icce */}
+
+      <CustomStack
+        flexDirection={"column"}
+        width="100%"
+        alignItems="center"
+        padding="43px 0"
+        bgcolor={palette.whites[100]}
+        gap="25px"
+      >
+        <Typography variant="body1" color="initial">
+          Three Levels Of ICCE Certification
+        </Typography>
+
+        <CustomStack >
+        
+        </CustomStack>
+      </CustomStack>
+
+      {/** icce recognition */}
+      <Footer />
     </Container>
   );
 };
