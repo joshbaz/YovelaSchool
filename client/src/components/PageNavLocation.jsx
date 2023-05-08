a2e6669e64245fba044d8912469e95f5bf9575aa
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2023-05-08 18:40:31 +0300

    component creation

---
+++ client/src/components/PageNavLocation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CustomStack from "./CustomStack";
+import { useTheme, Box, Typography } from "@mui/material";
+const PageNavLocation = () => {
+    const { palette } = useTheme();
+  return (
+    <div>PageNavLocation</div>
+  )
+}
+
+export default PageNavLocation
