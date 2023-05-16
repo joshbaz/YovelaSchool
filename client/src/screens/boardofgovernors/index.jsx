e18cae31d9161e166b0853fbc9b9e74bfff28a8e
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2023-05-17 00:20:46 +0300

    board updates

--- client/src/screens/boardofgovernors/index.jsx
+++ client/src/screens/boardofgovernors/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { styled } from "@mui/system";
-import { Box, Stack, useTheme, Typography } from "@mui/material";
+import { Box, Stack, useTheme, Typography, Divider } from "@mui/material";
 import PageHeader from "../../components/PageHeader";
 import Footer from "../../components/Footer";
 import { Icon } from "@iconify/react";
@@ -31,29 +31,40 @@ const BoardList = [
   {
     title: "Ministry of Education Collaboration & Registration",
     images: BoardMember1,
+    Name: "Dr. Oboyo Oludhe",
   },
   {
-    title: "Sports",
+    title: "Finance Management",
     images: BoardMember2,
+    Name: "Dr. Oboyo Oludhe",
   },
   {
-    title: "Music",
+    title: "Infrastructure development",
     images: BoardMember3,
+    Name: "Dr. Oboyo Oludhe",
+  },
+  {
+    title: "Infrastructure development",
+    images: BoardMember3,
+    Name: "Dr. Oboyo Oludhe",
   },
 ];
 
 const AdminList = [
   {
-    title: "Ministry",
+    title: "School Principal",
     images: AdminMember1,
+    Name: "Dr. Oboyo Oludhe",
   },
   {
-    title: "Sports",
+    title: "Deputy School Principal",
     images: AdminMember2,
+    Name: "Oboyo Oludhe",
   },
   {
-    title: "Music",
+    title: "Secretary",
     images: AdminMember3,
+    Name: "Dr. Oboyo Oludhe",
   },
 ];
 
@@ -67,9 +78,15 @@ const Container = styled(Stack)(({ theme }) => ({
   ".openSpanText": {
     fontWeight: "400",
   },
+  ".swiper2": {
+    backgroundColor: theme.palette.whites[100],
+    position: "relative",
+  },
 }));
 const BoardOfGovernors = () => {
   const { palette } = useTheme();
+  const navigationPrevRef = React.useRef(null);
+  const navigationNextRef = React.useRef(null);
   return (
     <Container gap={"0px"}>
       <CustomStack flexDirection={"column"} height="100% ">
@@ -132,10 +149,10 @@ const BoardOfGovernors = () => {
       {/** board of governors */}
       <CustomStack
         bgcolor={palette.whites[100]}
-        flexDirection="row"
+        flexDirection="column"
         gap="67px"
         justifyContent={"center"}
-        padding={"75px 0"}
+        padding={"71px 47px 20px 47px"}
         alignItems={"center"}
       >
         <Typography
@@ -145,8 +162,206 @@ const BoardOfGovernors = () => {
         >
           Board of Govenors
         </Typography>
+
+        <CustomStack flexDirection={"column"} position="relative" width="100%">
+          <CustomStack padding={"0px 28px"} width="100%">
+            <Swiper
+              scrollbar={{ hide: true }}
+              slidesPerView={3}
+              spaceBetween={20}
+              className="mySwiper swiper2"
+            >
+              {BoardList.map((data, index) => {
+                return (
+                  <SwiperSlide key={index} className="swiper2">
+                    <CustomStack
+                      bgcolor={palette.whites[100]}
+                      flexDirection={"column"}
+                      alignItems={"center"}
+                      gap="20px"
+                      overflow={"hidden"}
+                    >
+                      <Box
+                        sx={{
+                          height: "438px",
+                          width: "404.11px",
+                          position: "relative",
+                        }}
+                      >
+                        <img src={data.images} alt="hero" />
+                      </Box>
+                      <CustomStack flexDirection="column">
+                        <Typography
+                          variant="body1"
+                          color={palette.tertiary[500]}
+                          sx={{
+                            fontSize: "22px",
+                            fontWeight: "600",
+                            textTransform: "capitalize",
+                          }}
+                        >
+                          {data.Name}
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          color={palette.tertiary[500]}
+                          sx={{
+                            fontSize: "17px",
+                            fontWeight: "400",
+                            textTransform: "capitalize",
+                          }}
+                        >
+                          {data.title}
+                        </Typography>
+                      </CustomStack>
+                    </CustomStack>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </CustomStack>
+
+          <Box
+            height="50px"
+            width="50px"
+            className="..swiper-button-prev"
+            ref={navigationPrevRef}
+            zIndex={"10"}
+            sx={{
+              position: "absolute",
+              top: "38%",
+              left: "-1rem",
+              color: palette.secondary[700],
+            }}
+          >
+            <Icon icon="bi:arrow-left-short" width="50px" height="50px" />
+          </Box>
+          <Box
+            height="50px"
+            width="50px"
+            className="..swiper-button-next"
+            ref={navigationNextRef}
+            zIndex={"10"}
+            sx={{
+              position: "absolute",
+              top: "38%",
+              color: palette.secondary[700],
+              right: "-1rem",
+            }}
+          >
+            <Icon icon="bi:arrow-right-short" width="50px" height="60px" />
+          </Box>
+        </CustomStack>
       </CustomStack>
 
+      {/** Administrators */}
+      <CustomStack
+        bgcolor={palette.whites[100]}
+        flexDirection="column"
+        gap="44px"
+        justifyContent={"center"}
+        padding={"75px 47px"}
+        alignItems={"center"}
+      >
+        <Box width="100%" borderTop={`1px solid ${palette.tertiary[50]}`} />
+        <Typography
+          variant="h1"
+          color={palette.secondary[700]}
+          sx={{ fontWeight: "500", fontSize: "30px" }}
+        >
+          School Administration
+        </Typography>
+
+        <CustomStack flexDirection={"column"} position="relative" width="100%">
+          <CustomStack padding={"0px 28px"} width="100%">
+            <Swiper
+              scrollbar={{ hide: true }}
+              slidesPerView={3}
+              spaceBetween={20}
+              className="mySwiper swiper2"
+            >
+              {AdminList.map((data, index) => {
+                return (
+                  <SwiperSlide key={index} className="swiper2">
+                    <CustomStack
+                      bgcolor={palette.whites[100]}
+                      flexDirection={"column"}
+                      alignItems={"center"}
+                      gap="20px"
+                      overflow={"hidden"}
+                    >
+                      <Box
+                        sx={{
+                          height: "438px",
+                          width: "404.11px",
+                          position: "relative",
+                        }}
+                      >
+                        <img src={data.images} alt="hero" />
+                      </Box>
+                      <CustomStack flexDirection="column">
+                        <Typography
+                          variant="body1"
+                          color={palette.tertiary[500]}
+                          sx={{
+                            fontSize: "22px",
+                            fontWeight: "600",
+                            textTransform: "capitalize",
+                          }}
+                        >
+                          {data.Name}
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          color={palette.tertiary[500]}
+                          sx={{
+                            fontSize: "17px",
+                            fontWeight: "400",
+                            textTransform: "capitalize",
+                          }}
+                        >
+                          {data.title}
+                        </Typography>
+                      </CustomStack>
+                    </CustomStack>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </CustomStack>
+
+          <Box
+            height="50px"
+            width="50px"
+            className="..swiper-button-prev"
+            ref={navigationPrevRef}
+            zIndex={"10"}
+            sx={{
+              position: "absolute",
+              top: "38%",
+              left: "-1rem",
+              color: palette.secondary[700],
+            }}
+          >
+            <Icon icon="bi:arrow-left-short" width="50px" height="50px" />
+          </Box>
+          <Box
+            height="50px"
+            width="50px"
+            className="..swiper-button-next"
+            ref={navigationNextRef}
+            zIndex={"10"}
+            sx={{
+              position: "absolute",
+              top: "38%",
+              color: palette.secondary[50],
+              right: "-1rem",
+            }}
+          >
+            <Icon icon="bi:arrow-right-short" width="50px" height="60px" />
+          </Box>
+        </CustomStack>
+      </CustomStack>
       <Footer />
     </Container>
   );
