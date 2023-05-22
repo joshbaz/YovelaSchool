c3d54ba9282f2bb86e5b2e3c287f028e09d9c9fb
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2023-05-22 16:48:05 +0300

    update

--- client/src/screens/home/index.jsx
+++ client/src/screens/home/index.jsx
@@ -411,6 +411,19 @@ const Home = () => {
                 swiper.navigation.init();
                 swiper.navigation.update();
               }}
+              onSwiper={(swiper) => {
+                // Delay execution for the refs to be defined
+                setTimeout(() => {
+                  // Override prevEl & nextEl now that refs are defined
+                  swiper.params.navigation.prevEl = navigationPrevRef.current;
+                  swiper.params.navigation.nextEl = navigationNextRef.current;
+
+                  // Re-init navigation
+                  swiper.navigation.destroy();
+                  swiper.navigation.init();
+                  swiper.navigation.update();
+                });
+              }}
             >
               <SwiperSlide>
                 <CustomStack
